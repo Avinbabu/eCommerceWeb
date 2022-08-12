f3c593d92c22358125b2e98ec8b31d5d05221085
AvinBabuPS <avinbabu007@gmail.com>
2022-08-12 15:21:24 +0530

    json data showed as cards

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-fetch('https://reqres.in/api/users?page=2')
-  .then(response => response.text())
-  .then(data => console.log(data));
+let http = new XMLHttpRequest();
+http.open('get', 'user.json', true);
+http.send();
+http.onload = function(){
+   if(this.readyState == 4 && this.status == 200){
+      let user = JSON.parse(this.responseText);
+      let output = "";
+      for(let item of user){
+        output += `
+          <div class="card">
+            <img src="${item.avatar}" alt="${item.id}">
+            <h5 class="card-title"><b>${item.first_name} ${item.last_name}</b></h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">User id :${item.id}</li>
+              <li class="list-group-item">Email :${item.email}</li>
+            </ul>
+            <a href="#" class="btn btn-dark">READ MORE</a>
+          </div>
+         `;
+      }
+      document.querySelector(".cardgroup").innerHTML = output;
+   }
+}
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+  
