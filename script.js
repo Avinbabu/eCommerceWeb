4084aa497ed5855f69f9e228b98ab7e317e7632a
AvinBabuPS <avinbabu007@gmail.com>
2022-08-16 17:01:10 +0530

    added popup for read more

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+let users1
 let http = new XMLHttpRequest();
-http.open('get', 'user.json', true);
+http.open('get', "https://reqres.in/api/users?page=2", true);
 http.send();
 http.onload = function(){
    if(this.readyState == 4 && this.status == 200){
       let user = JSON.parse(this.responseText);
+      // console.log(user.data)
+      users1 = user.data
       let output = "";
-      for(let item of user){
+      for(let item of users1){
         output += `
           <div class="card">
             <img src="${item.avatar}" alt="${item.id}">
@@ -14,13 +17,63 @@ http.onload = function(){
               <li class="list-group-item">User id : ${item.id}</li>
               <li class="list-group-item">Email :<a> ${item.email} </a></li>
             </ul>
-            <a href="${item.avatar}" class="btn btn-dark">READ MORE</a>
+            <button onclick="showuser(${item.id})"  class="btn btn-dark" >
+            READ MORE</button>
           </div>
          `;
       }
       document.querySelector(".cardgroup").innerHTML = output;
+      
    }
 }
+function showuser(id){
+  document.getElementById('popup').style.display='block'
+  for (let items of users1){
+    if (items.id == id){
+      document.getElementById('avatar_').src = items.avatar
+      document.getElementById('userName').innerHTML = items.first_name + " " + items.last_name
+      document.getElementById('email_id').innerHTML = "Email : " + items.email
+      document.getElementById('user_id').innerHTML = "User Id : " + items.id
+      
+      // console.log(items)
+
+      
+
+    }
+    
+    
+
+  }
+
+}
+
+// When the user clicks on <span> (x), close the modal
+ function closePopup() {
+  document.getElementById('popup').style.display = 'none'
+}
+
+
+
+  function search_user() {
+    document.getElementById('popup2').style.display = 'block'
+    let input = document.getElementById('searchbar').value
+    for (let items of users1){
+      if (input == items.id || input == items.first_name ||input == items.last_name ||input == items.email){
+        console.log(items.first_name)
+        document.getElementById('avatar_2').src = items.avatar
+        document.getElementById('userName2').innerHTML = items.first_name + " " + items.last_name
+        document.getElementById('email_id2').innerHTML = "Email : " + items.email
+        document.getElementById('user_id2').innerHTML = "User Id : " + items.id
+
+      }
+    }
+  }
+
+  function closePopup2(){
+    document.getElementById('popup2').style.display = 'none'
+    document.getElementById('searchbar').value = ''
+  }
+
 
 
 
